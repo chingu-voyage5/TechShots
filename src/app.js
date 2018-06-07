f2f4c7a6940474d955d45645f224ece42f422cac
Deyl <soulvisman@gmail.com>
2018-06-07 14:25:37 +0600

    Getting post from db

--- src/app.js
+++ src/app.js
@@ -5,14 +5,21 @@ require('./config/config');
 
 const app = express();
 
+const { Post } = require('./db/models/Post');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('home', {
-        title: 'TechShots',
-        content: 'lorem ipsum and things like that...'
-    });
+    Post
+        .find()
+        .then((posts) => {
+            res.render('home', {
+                title: 'TechShots',
+                posts
+            });
+        })
+        .catch(console.log)
 });
 
 app.listen(3000, () => {
