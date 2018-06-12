961f93a92c62216f895d972a5600c15597d929bb
Deyl <soulvisman@gmail.com>
2018-06-13 05:57:43 +0600

    Updating src/config/config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 const env = process.env.NODE_ENV || 'dev';
 
+const newsAPI = 'YOUR_NEWSAPI';
+
 if (env === 'dev'){
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TechShots';
 } else if (env === 'test'){
