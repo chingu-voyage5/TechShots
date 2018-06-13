5ed18220e43052ad4511970616a41cf3f9243aa9
Deyl <soulvisman@gmail.com>
2018-06-13 08:17:06 +0600

    Hiding config/config.js

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV || 'dev';
-
-process.env.NEWSAPI = '';
-
-if (env === 'dev'){
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TechShots';
-} else if (env === 'test'){
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TechShotsTest'
-}
