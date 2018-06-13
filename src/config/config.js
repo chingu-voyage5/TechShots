e0c937a3ceda3c019b51ad550c820dc56409437f
Deyl <soulvisman@gmail.com>
2018-06-14 05:31:16 +0600

    Excluding NEWSAPI from config/config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,5 @@
 const env = process.env.NODE_ENV || 'dev';
 
-const newsAPI = 'YOUR_NEWSAPI';
-
 if (env === 'dev'){
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TechShots';
 } else if (env === 'test'){
