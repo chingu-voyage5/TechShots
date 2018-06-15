b5aa52bc58de71322338f00288fc4625e95fc895
Deyl <soulvisman@gmail.com>
2018-06-15 06:20:15 +0600

    The config file was deleted

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-const env = process.env.NODE_ENV || 'dev';
-
-if (env === 'dev'){
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TechShots';
-} else if (env === 'test'){
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TechShotsTest'
-}
