613ff873cf1fffc0e83b8210a40ae86e958a8f6e
Chetan Yadav <imchetanyadav@gmail.com>
2018-06-19 16:19:50 +0530

    Removed unnecessary require statement

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const NewsAPI = require('newsapi');
 
-require('./config/config');
+// require('./config/config');
 require('dotenv').config();
 
 const app = express();
